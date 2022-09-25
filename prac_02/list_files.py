dc6198302a49ddd460dfec8005b4e8045edddf11
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2022-09-25 21:44:40 +1000

    Add list_files.py from GitHub - prac_02

---
+++ prac_02/list_files.py
@@ -0,0 +1,12 @@
+"""
+CP1404 - Practical 2
+Program - code given by prac
+"""
+
+import os
+
+print(f"The files and folders in {os.getcwd()} are:")
+items = os.listdir('.')
+for item in items:
+    prefix = "(d) " if os.path.isdir(item) else "(f) "
+    print(f"{prefix}\t{item}")
