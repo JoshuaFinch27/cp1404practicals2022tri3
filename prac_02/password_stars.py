98919e4391bb8babc7dc6f277186de2393d04211
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2022-09-25 21:55:42 +1000

    Update password_stars.py with functions and clean up (commit) old files - Lyrics.zip, sort_files_1.py and sort_files_2.py

--- prac_02/password_stars.py
+++ prac_02/password_stars.py
@@ -5,8 +5,24 @@ Program to ask user for a password with error checking
 
 MINIMUM_LENGTH = 6
 
-password = str(input("Password: "))
-while len(password) < MINIMUM_LENGTH:
-    print("Invalid - Password is too short.")
+
+def main():
+    """Asks user for a password with error checking"""
+    password = get_password()
+    while len(password) < MINIMUM_LENGTH:
+        print("Invalid - Password is too short.")
+        password = get_password()
+    print_password_as_stars(password)
+
+
+def print_password_as_stars(password):
+    print(len(password) * "*")
+
+
+def get_password():
+    """Function gets password input from user"""
     password = str(input("Password: "))
-print(len(password) * "*")
+    return password
+
+
+main()
