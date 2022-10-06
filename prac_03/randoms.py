b4825012d7907a4f9da9a5aa6ed1815750d9cdeb
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2022-10-06 13:53:13 +1000

    Add completed randoms.py

---
+++ prac_03/randoms.py
@@ -0,0 +1,27 @@
+"""
+CP1404 - Practical 3
+Program that: Has answers to questions and has random number exercise
+"""
+
+import random
+
+# Line 1:
+#    What I saw: 11
+#    The smallest number possible: 5
+#    The largest number possible: 20
+
+# Line 2:
+#   What I saw: 3
+#   Smallest: 3
+#   Largest: 9
+#   Is 4 possible: No
+
+# Line 3:
+#   What I saw: 4.515523624983908
+#   Smallest: 2.5
+#   Largest: 4.5
+
+print(random.randint(1, 100))
+
+
+
