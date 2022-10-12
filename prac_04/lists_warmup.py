98b66a281796617554381288644b667aee7c6812
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2022-10-12 22:11:35 +1000

    Add complete lists_warmup.py

---
+++ prac_04/lists_warmup.py
@@ -0,0 +1,45 @@
+"""
+CP1404 - Practical 4
+Program that: practise using lists
+"""
+
+numbers = [3, 1, 4, 1, 5, 9, 2]
+
+# numbers[0]
+# = 3
+
+# numbers[-1]
+# = 2
+
+# numbers[3]
+# = 1
+
+# numbers[:-1]
+# = [3, 1, 4, 1, 5, 9]
+
+# numbers[3:4]
+# = [1]
+
+# 5 in numbers
+# = True
+
+# 7 in numbers
+# = False
+
+# "3" in numbers
+# = False
+
+# numbers + [6, 5, 3]
+# = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+print(numbers)
+numbers[0] = "ten"
+print(numbers)
+
+print(numbers)
+numbers[-1] = 1
+print(numbers)
+
+print(numbers[2:])
+
+print(9 in numbers)
