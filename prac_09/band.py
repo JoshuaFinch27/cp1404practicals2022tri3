29383e46a017104fd2984455ebc01faa5810507e
JoshuaFinch27 <joshua.finch@my.jcu.edu.au>
2023-01-23 15:33:57 +1000

    Add incomplete band.py

---
+++ prac_09/band.py
@@ -0,0 +1,28 @@
+"""
+CP1404 Prac 9 - Band
+
+
+
+Stuck, need to move on.
+"""
+
+
+class Band:
+    """Band Class"""
+
+    def __init__(self, name=""):
+        """Construct a Band with a name and empty musician list."""
+        self.name = name
+        musicians = []
+
+    def __str__(self):
+        """Return a string representation of a Band."""
+        # return f"{self.name} ({musicians} ({}))"
+
+
+    def add(self):
+
+
+    def play(self):
+
+
